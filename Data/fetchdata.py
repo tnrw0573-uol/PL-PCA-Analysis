1d362f0c852aa0ef80216e64ec00ba7f22e1e9d4
tnrw0573-uol <tnrw0573@leeds.ac.uk>
2026-08-29 15:17:50 +0000

    Changed plan to PCA on Premier League players due to complexities on fetching the data for the linear regression project.

--- Data/fetchdata.py
+++ Data/fetchdata.py
@@ -1,20 +1,24 @@
 import requests
 from pprint import pprint
 
-url = "https://api.football-data.org/v4/competitions/PL/teams"
+url = "https://api.thestatsapi.com/api/football"
 headers = {
-    'X-Auth-Token': '3d6a7644f1094a0e8e4a5ce7ed1bcfca',
+    'Authorization': 'Bearer fapi_RiFdBS9odearwe8WgyoXfljk9mUo9G9I',
     'Content-Type': 'application/json'
 }
+params = {
+    'name': 'Premier League',
+    'country': 'England'
+}
 
-all_data = {}
-for season in ['2023', '2024', '2025']:
-    params = {'season': season}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        all_data[season] = data
-    else:
-        print(f"Request failed with status code {response.status_code}")
-
-pprint(all_data['2025'])
+#Find Premier League ID
+response = requests.get(f"{url}/competitions", headers=headers, params=params)
+if response.status_code == 200:
+    content = response.json()
+    data = content['data']
+    for league in data:
+        if league['name'] == 'Premier League':
+            season_id = league['id']
+            print(season_id)
+else:
+    print(f"Request failed with error code {response.status_code}.")
