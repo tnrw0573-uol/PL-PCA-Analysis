38ebf909317b0b1fbe39ee9b8d63a4067a656ea7
tnrw0573-uol <tnrw0573@leeds.ac.uk>
2026-08-29 15:25:48 +0000

    Split process of finding Premier League ID into 2 functions: the fetching itself and checking if the response was succesful.

--- Data/fetchdata.py
+++ Data/fetchdata.py
@@ -1,24 +1,30 @@
 import requests
 from pprint import pprint
 
-url = "https://api.thestatsapi.com/api/football"
+base_url = "https://api.thestatsapi.com/api/football"
 headers = {
     'Authorization': 'Bearer REMOVED',
     'Content-Type': 'application/json'
 }
-params = {
-    'name': 'Premier League',
-    'country': 'England'
-}
+
+#Check response and handle errors
+def response_success(response):
+    if response.status_code == 200:
+        print("Successful response.")
+        return True
+    else:
+        print(f"Response unsuccessful with status code {response.status_code}")
+    return False
 
 #Find Premier League ID
-response = requests.get(f"{url}/competitions", headers=headers, params=params)
-if response.status_code == 200:
-    content = response.json()
-    data = content['data']
-    for league in data:
-        if league['name'] == 'Premier League':
-            season_id = league['id']
-            print(season_id)
-else:
-    print(f"Request failed with error code {response.status_code}.")
+def find_pl_id(base_url, headers):
+    response = requests.get(f"{base_url}/competitions", headers=headers, params={'name': 'Premier League', 'country': 'England'})
+    if response_success(response) == True:
+        content = response.json()
+        data = content['data']
+        for league in data:
+            if league['name'] == 'Premier League':
+                season_id = league['id']
+                print(season_id)
+
+find_pl_id(base_url, headers)
