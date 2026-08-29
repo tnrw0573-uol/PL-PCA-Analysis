2ae3852f6cf6b6ad31d6e9c4c7b0021007cf8b09
tnrw0573-uol <tnrw0573@leeds.ac.uk>
2026-08-29 15:33:00 +0000

    Noticed season_id was the wrong name for the variable, so changed the name to league_id.

--- Data/fetchdata.py
+++ Data/fetchdata.py
@@ -17,15 +17,15 @@ def response_success(response):
     return False
 
 #Find Premier League ID
-season_id = ''
-def find_pl_id(base_url, headers, season_id):
+league_id = ''
+def find_pl_id(base_url, headers, league_id):
     response = requests.get(f"{base_url}/competitions", headers=headers, params={'name': 'Premier League', 'country': 'England'})
     if response_success(response) == True:
         content = response.json()
         data = content['data']
         for league in data:
             if league['name'] == 'Premier League':
-               season_id = league['id']
-    return season_id 
-season_id = find_pl_id(base_url, headers, season_id)
-print(season_id)
+               league_id = league['id']
+    return league_id 
+league_id = find_pl_id(base_url, headers, league_id)
+print(league_id)
