b9b7bc7e52b0b35c34057c6b6d20a56129d5e5fd
tnrw0573-uol <tnrw0573@leeds.ac.uk>
2026-08-31 15:27:29 +0000

    Extended PCA file by allowing user to choose positions to run PCA on.

--- Analysis/PCA.py
+++ Analysis/PCA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 #Read stats CSV 
 try:
@@ -22,16 +22,88 @@ data = pca.fit_transform(norm_df)
 reduced_df = pd.DataFrame(data=data, columns=['PC1', 'PC2'])
 new_df = pd.concat([df[['player_id']], df[['name']], df[['position']], reduced_df], axis = 1)
 
-#Plot
-plt.clf() #Prevent new figure from being created
-positions = list(set(new_df['position']))
-colors = ['r', 'g', 'b', 'y']
-for position, color in zip(positions, colors):
-    cond = new_df['position'] == position
-    plt.scatter(new_df.loc[cond, 'PC1'], new_df.loc[cond, 'PC2'], c=color, s=50)
-plt.legend(positions, title="Position", loc='best')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.title("PCA on Premier League Players")
-plt.savefig('Analysis/PL_PCA_scatter.png')
-plt.show()
+def plot_scatter(figure, condition):
+    figure.add_trace(go.Scatter(
+                x=new_df.loc[condition, 'PC1'],
+                y=new_df.loc[condition, 'PC2'],
+                mode='markers',
+                marker=dict(size=8),
+                text=new_df.loc[condition, 'name'], 
+                hovertemplate='<b>%{text}</b><extra></extra>'
+            ))
+
+#Get user input and validate
+valid_options = ['a', 'g', 'd', 'm', 'f']
+while True:
+    option = input("For what positions do you want PCA analysis? Enter 'a' for all, 'd' for defenders, 'm' for midfielders and 'f' for forwards: ")
+    if option in valid_options:
+        break
+    else:
+        print(f"Only valid inputs are {valid_options}")
+
+if option.lower() == "a":
+    #Plot for all players
+    fig = go.Figure()
+    positions = list(set(new_df['position']))
+    colors = ['red', 'green', 'blue', 'yellow']
+    for position, color in zip(positions, colors):
+        cond = new_df['position'] == position
+        plot_scatter(fig, cond)
+        fig.add_trace(go.Scatter(
+            name = f"{position}",
+            marker=dict(color=color, size=8),
+        ))
+    fig.update_layout(
+        title='PCA Visualization on all Premier League Players',
+        xaxis_title='Principal Component 1',
+        yaxis_title='Principal Component 2'
+    )
+    fig.show()
+
+elif option == "g":
+    #Plot for goalkeepers
+    fig = go.Figure()
+    cond = new_df['position'] == "G"
+    plot_scatter(fig, cond)
+    fig.update_layout(
+            title='PCA Visualization on all Premier League Goalkeepers',
+            xaxis_title='Principal Component 1',
+            yaxis_title='Principal Component 2'
+        )
+    fig.show()
+
+elif option == "d":
+    #Plot for defenders
+    fig = go.Figure()
+    cond = new_df['position'] == "D"
+    plot_scatter(fig, cond)
+    fig.update_layout(
+            title='PCA Visualization on all Premier League Defenders',
+            xaxis_title='Principal Component 1',
+            yaxis_title='Principal Component 2'
+        )
+    fig.show()
+
+elif option == "m":
+    #Plot for midfielders
+    fig = go.Figure()
+    cond = new_df['position'] == "M"
+    plot_scatter(fig, cond)
+    fig.update_layout(
+            title='PCA Visualization on all Premier League Midfielders',
+            xaxis_title='Principal Component 1',
+            yaxis_title='Principal Component 2'
+        )
+    fig.show()
+
+elif option == "f":
+    #Plot for forwards
+    fig = go.Figure()
+    cond = new_df['position'] == "F"
+    plot_scatter(fig, cond)
+    fig.update_layout(
+            title='PCA Visualization on all Premier League Forwards',
+            xaxis_title='Principal Component 1',
+            yaxis_title='Principal Component 2'
+        )
+    fig.show()
