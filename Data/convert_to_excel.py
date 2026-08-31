ce4ce881bec958900c5d539782444ba78a8f2780
tnrw0573-uol <tnrw0573@leeds.ac.uk>
2026-08-31 13:41:56 +0000

    Dropped some stats as they were 0 for all players which can't be true.

--- Data/convert_to_excel.py
+++ Data/convert_to_excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('Data/player_stats.csv')
+df = df.drop(['big_chances_missed', 'aerial_duels_won_percentage', 'successful_dribbles_percentage'], axis = 1)
 df.to_excel('Data/player_stats.xlsx', index = False)
